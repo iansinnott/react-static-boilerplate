e6db505acc7d6a7cc05a1bb4e18b95f64361ff44
Ian Sinnott <ian@iansinnott.com>
2016-01-03 19:52:48 -0800

    Add routes

--- client/components/App.js
+++ client/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import 'normalize.css';
 
 // TDOO: Add font awesome
@@ -6,11 +6,20 @@ import 'normalize.css';
 import s from './App.styl';
 import logo from './react-logo.png';
 
-// Favicon link is created server-side, this is just make webpack package it
+// Favicon link is created server-side, this just makes webpack package it
 import './favicon.ico';
 
-const ReactBoilerplate = props => (
-  <div className={s.ReactBoilerplate}>
+export const About = props => (
+  <div className={s.About}>
+    <div className={s.siteTitle}>
+      <h1>About</h1>
+    </div>
+    <p>Welcome, to about us.</p>
+  </div>
+);
+
+export const Home = props => (
+  <div className={s.Home}>
     <div className={s.siteTitle}>
       <img src={logo} alt='React Logo' />
       <h1>React Static Boilerplate</h1>
@@ -24,18 +33,25 @@ const ReactBoilerplate = props => (
   </div>
 );
 
+export const NotFound = props => (
+  <div className={s.page}>
+    <h4>Not found</h4>
+  </div>
+);
+
 /**
  * NOTE: As of 2015-11-09 react-transform does not support a functional
  * component as the base compoenent that's passed to ReactDOM.render, so we
- * still use create class here.
- *
- * @see README for details.
+ * still use createClass here.
  */
 export const App = React.createClass({
+  propTypes: {
+    children: PropTypes.any,
+  },
   render() {
     return (
-      <div>
-        <ReactBoilerplate />
+      <div className={s.App}>
+        {this.props.children}
       </div>
     );
   },
