34079cc219f0d95d150d6bc4d2cbabdccbe4fb1b
Ian Sinnott <ian@iansinnott.com>
2016-02-08 23:12:36 -0800

    Remove unecessary stuff

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,7 +4,6 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var axis = require('axis');
 var rupture = require('rupture');
-var StaticSitePlugin = require('react-static-webpack-plugin');
 
 module.exports = {
   devtool: 'source-map',
@@ -16,7 +15,6 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public'),
     filename: '[name].js',
-    libraryTarget: 'umd',
     publicPath: '/',
   },
 
@@ -31,11 +29,6 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin({
       screw_ie8: true,
       compressor: { warnings: false },
-    }),
-    new StaticSitePlugin({
-      src: 'app',
-      stylesheet: '/app.css',
-      favicon: '/favicon.ico',
     }),
   ],
 
