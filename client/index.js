16bdf9f36c900e1f63fbe590c010d2bd7dc82959
Ian Sinnott <ian@iansinnott.com>
2016-05-10 23:56:56 -0700

    Remove empty line

--- client/index.js
+++ client/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 
-
 // Import your routes so that you can pass them to the <Router /> component
 import routes from './routes.js';
 
