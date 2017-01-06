58b851fe54baaaf9974a8c4cd380bf13fda7c90d
Ian Sinnott <ian@iansinnott.com>
2017-01-05 16:06:11 -0800

    Upgrade app-time and use manifest in template

--- template.js
+++ template.js
@@ -1,35 +1,32 @@
+/* eslint-disable react/no-danger */
 const React = require('react');
 const T = React.PropTypes;
 
-const Html = ({
-  title = 'Rainbow Unicorns',
-  bundle = 'app.js',
-  body = '',
-  favicon = 'favicon.ico',
-  stylesheet = 'app.css',
-}) => (
-  <html lang='en'>
-    <head>
-      <meta charSet='utf-8' />
-      <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <title>{title}</title>
-      {favicon ? <link rel='shortcut icon' href={favicon} /> : null}
-      {stylesheet ? <link rel='stylesheet' href={stylesheet} /> : null}
-    </head>
-    <body>
-      <div id='root' dangerouslySetInnerHTML={{ __html: body }} />
-      <script src={bundle} />
-    </body>
-  </html>
-);
+const Html = ({ title = 'Amazing Default Title', body, manifest }) => {
+  return (
+    <html lang='en'>
+      <head>
+        <meta charSet='utf-8' />
+        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <title>{title}</title>
+        <link rel='stylesheet' href={manifest['app.css']} />
+
+        {/* If you have a favicon file you can add by modifying the next line  */}
+        {/* <link rel='shortcut icon' href={manifest['favicon.ico']} /> */}
+      </head>
+      <body>
+        <div id='root' dangerouslySetInnerHTML={{ __html: body }} />
+        <script src={manifest['app.js']} />
+      </body>
+    </html>
+  );
+};
 
 Html.propTypes = {
   title: T.string,
-  bundle: T.string,
   body: T.string,
-  favicon: T.string,
-  stylesheet: T.string,
+  manifest: T.object.isRequired,
 };
 
 module.exports = Html;
